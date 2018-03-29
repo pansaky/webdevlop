87e6e63e7d59cb69d92ca636e78c0650cdd27742
pansaky@sina.cn <pansaky@sina.cn>
2018-03-29 22:09:20 +0800

    update

--- babys/babydisplay/views.py
+++ babys/babydisplay/views.py
@@ -4,8 +4,8 @@ from . import models
 
 # Create your views here.
 def baby(request):
-    dbmodel = models.dbmodel.from_db()
-    return render(request, 'root.html', {'mod', dbmodel})
+    #dbmodel = models.dbmodel.from_db()
+    return render(request, 'root.html')
 
 def wife(request):
     return render(request, 'root.html', {'TOB': 'Love my baby!'})
